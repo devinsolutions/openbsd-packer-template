386c7f03001201bed224ff8246c62f8923ae0eff
Adam Dej <adam@dej.tech>
2019-10-31 12:05:43 +0100

    Add post-processors for image creation on GCE

--- scripts/prep-gce.sh
+++ scripts/prep-gce.sh
@@ -12,4 +12,7 @@ sed -i -e 's/^tty00[[:space:]]\(.*\)[[:space:]]unknown off$/tty00   \1   vt220  
 
 # GCE packages
 pkg_add google-compute-engine
+## If we enabled these now, shutdown scripts would freeze since this service wouldn't be able to
+## make contact with GCE services. Let's enable them the first time we boot on GCE instead.
 echo 'rcctl enable google_compute_engine' > /etc/rc.firsttime
+echo 'rcctl start google_compute_engine' >> /etc/rc.firsttime
